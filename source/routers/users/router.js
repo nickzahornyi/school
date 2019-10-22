26a282712ba0dffd5f142f57f28947da2ce94394
Geo <geo@syneforge.com>
2019-10-22 17:37:20 +0300

    add midlewares

--- source/routers/users/router.js
+++ source/routers/users/router.js
@@ -2,11 +2,13 @@ import express from 'express';
 
 import { get, post } from './index';
 import { getByHash, updateByHash, deleteByHash } from './hash';
+import { limiter, validator } from '../../utils';
+import { createUser } from '../../schemas';
 
 export const router = express.Router();
 
-router.get('/', get);
-router.post('/', post);
+router.get('/', [limiter(2, 1000 * 60)], get);
+router.post('/', [validator(createUser)], post);
 
 router.get('/:userHash', getByHash);
 router.put('/:userHash', updateByHash);
