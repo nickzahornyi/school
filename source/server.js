16447d307a145b2b97339fe504464b41e3284804
Nick Zahornyi <nick_surveyor@ukr.net>
2019-10-24 22:28:49 +0300

    task-1 error logger

--- source/server.js
+++ source/server.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 
 //Routers
 import * as routers from './routers';
+import { errorLogger } from './utils/errorLogger';
 
 const app = express();
 
@@ -13,5 +14,11 @@ app.use('/', routers.auth);
 app.use('/users', routers.users);
 app.use('/classes', routers.classes);
 app.use('/lessons', routers.lessons);
+
+// eslint-disable-next-line no-unused-vars
+app.use((error, req, res, next) => {
+    errorLogger(error);
+    res.status(500).json({ message: error.message });
+});
 
 export { app };
