fc98890824e74eec9e4ce53f4e187340a207ca99
Nick Zahornyi <nick_surveyor@ukr.net>
2019-10-24 23:05:37 +0300

    task-3

--- source/server.js
+++ source/server.js
@@ -5,6 +5,7 @@ import bodyParser from 'body-parser';
 //Routers
 import * as routers from './routers';
 import { errorLogger } from './utils/errorLogger';
+import { NotFoundError } from './utils/errors';
 
 const app = express();
 
@@ -14,6 +15,10 @@ app.use('/', routers.auth);
 app.use('/users', routers.users);
 app.use('/classes', routers.classes);
 app.use('/lessons', routers.lessons);
+
+app.use((req, res, next) => {
+    next(new NotFoundError(`Not Found - method: ${req.method}, endpoint: ${req.url}`));
+});
 
 // eslint-disable-next-line no-unused-vars
 app.use((error, req, res, next) => {
